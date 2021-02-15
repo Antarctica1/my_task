0fbb04f6e052dc4dfef1261ace4ed3e294d2831e
penguin <jhh019176@gmail.com>
2021-02-16 07:45:22 +0900

    Revert "크롤링"
    This reverts commit 07293bc68e78a50d19bbe217bb9c4ca1a47d6c4e.

--- 크롤링/google_imaga.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-keyword=input("이미지:")
-url="https://www.google.co.kr/search?hl=ko&tbm=isch&q={}".format(keyword)
-driver = webdriver.Chrome("C:\chromedriver.exe")
-driver.implicitly_wait(15)
-driver.maximize_window()
-
-driver.get(url)
